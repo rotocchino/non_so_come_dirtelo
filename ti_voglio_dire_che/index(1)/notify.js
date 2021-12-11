ddf44b9f45279a40b2076cf009de7335ac557a91
rotocchino <rotoccotarocco@gmail.com>
2021-12-11 21:49:47 +0100

    per crush
    usatelo, vi servira'

---
+++ ti_voglio_dire_che/index(1)/notify.js
@@ -0,0 +1,7 @@
+var nome = window.prompt("inserisci il tuo nome");
+
+document.getElementById("amore").innerHTML = "❤️legggi tutto❤️_" + nome;
+
+document.getElementById("amore(2)").innerHTML = "ritieniti molto fortunata/o_" + nome;
+
+document.getElementById("amore(3)").innerHTML = "TI AMO_" + nome;
